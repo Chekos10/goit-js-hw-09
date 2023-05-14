9fb75702fbdc74be2813c16858287639effef50c
Chekos10 <119345370+Chekos10@users.noreply.github.com>
2023-05-14 16:52:52 +0000

    Deploying to gh-pages from @ Chekos10/goit-js-hw-09@dfca2f438bfe96a3e15440f894dc8e1969981828 🚀

---
+++ 01-color-switcher.b5dacfac.js
@@ -0,0 +1,2 @@
+!function(){var t={startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]")};t.startBtn.addEventListener("click",(function(t){t.target.disabled="true",intervalId=setInterval((function(){document.body.style.background="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),t.stopBtn.addEventListener("click",(function(n){clearInterval(intervalId),t.startBtn.disabled=""}))}();
+//# sourceMappingURL=01-color-switcher.b5dacfac.js.map
